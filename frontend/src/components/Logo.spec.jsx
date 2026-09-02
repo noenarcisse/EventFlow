bc1fd7fc3253bffdfdcb63bee636e20eb11b1b4e
Noe <narcisse.noe@gmail.com>
2026-09-02 12:03:04 +0200

    Update Logo.spec.jsx

--- frontend/src/components/Logo.spec.jsx
+++ frontend/src/components/Logo.spec.jsx
@@ -3,13 +3,19 @@ import { render, screen } from "@testing-library/react";
 import Logo from "./Logo";
 
 describe("Testing of the Logo component", () => {
-    test("Logo is loaded properly on the page", () => {
+    test("B1_Logo is loaded properly on the page", () => {
         render(<Logo></Logo>);
         const logo = screen.getByLabelText("eventflow");
         expect(logo).toBeInTheDocument();
     });
 
-    test("Logo is loaded properly on the page", () => {
+    test("B2_Logo is loaded properly on the page", () => {
+        render(<Logo></Logo>);
+        const logo = screen.getByLabelText("eventflow");
+        expect(logo).toHaveAttribute("aria-label");
+    });
+
+    test("B3_Logo is loaded properly on the page", () => {
         render(<Logo></Logo>);
         const logo = screen.getByLabelText("eventflow");
         expect(logo).toHaveAttribute("width", "34");
