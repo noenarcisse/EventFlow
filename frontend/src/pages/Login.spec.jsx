fddca5430bc07984093e413715c1e3c13e89b9a6
Noe <narcisse.noe@gmail.com>
2026-09-02 16:04:17 +0200

    Update Login.spec.jsx

--- frontend/src/pages/Login.spec.jsx
+++ frontend/src/pages/Login.spec.jsx
@@ -1,17 +1,39 @@
 import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
+import { AuthProvider } from "../auth";
+import { MemoryRouter, Router } from "react-router";
 
 describe("Testing of the Login page", () => {
     test("D5 Accessibilité issue", () => {
-        //         TypeError: Cannot destructure property 'login' of 'useAuth(...)' as it is null.
-        //  ❯ Login src/pages/Login.jsx:8:11
-        //       6|
-        //       7| export default function Login() {
-        //       8|   const { login, register } = useAuth();
+        //TestingLibraryElementError: Found a label with the text of: Email,
+        // however no form control was found associated to that label.
+        // Make sure you're using the "for" attribute or "aria-labelledby" attribute correctly.
 
-        // render(<Login></Login>);
-        // const logo = screen.getByLabelText("Email");
-        // expect(logo).toBeInTheDocument();
+        {
+            /* <label>
+                Email
+              </label>
+              <input
+                class="input"
+                required=""
+                type="email"
+                value="client@eventflow.test" */
+        }
+
+        render(
+            <MemoryRouter>
+                <AuthProvider>
+                    <Login />
+                </AuthProvider>
+            </MemoryRouter>,
+        );
+
+        const input = screen.getAllByRole("textbox", {
+            value: "client@eventflow.test",
+        });
+        const email = screen.getByLabelText("Email"); // ne peut pas recup
+        expect(email).toBeInTheDocument();
+        expect(input).toBeInTheDocument();
     });
 });
